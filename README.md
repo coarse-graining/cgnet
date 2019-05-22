e98c80bc5fa3b9a7dec6b2cbb9abc298b2a9a2a2
Brooke Husic <bhusic@stanford.edu>
2019-05-22 14:50:09 -0700

    added gitignore, initial readme and requirements and setup files

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# cgnet
+cgnet
+===
+
+In development!
+
+Requirements
+---
++ ``numpy``
++ ``pytorch``
++ ``scipy``
+
+Usage
+---
+
+Coming soon
+
+Cite
+---
+Based off the CGnet paper,
+
+```bibtex
+@article{wang2019machine,
+  title={Machine learning of coarse-grained molecular dynamics force fields},
+  author={Wang, Jiang and Olsson, Simon and Wehmeyer, Christoph and Pérez, Adrià and Charron, Nicholas E and de Fabritiis, Gianni and Noé, Frank and Clementi, Cecilia},
+  journal={ACS Central Science},
+  year={2019},
+  publisher={ACS Publications},
+  doi={10.1021/acscentsci.8b00913}
+}
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+NAME = 'cgnet'
+VERSION = '0.0'
+
+
+def read(filename):
+    import os
+    BASE_DIR = os.path.dirname(__file__)
+    filename = os.path.join(BASE_DIR, filename)
+    with open(filename, 'r') as fi:
+        return fi.read()
+
+def readlist(filename):
+    rows = read(filename).split("\n")
+    rows = [x.strip() for x in rows if x.strip()]
+    return list(rows)
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author="B Husic, Nick Charron, ...",
+    author_email="bhusic@stanford.edu",
+    url='https://github.com/coarse-graining/cgnet',
+    download_url='https://github.com/coarse-graining/cgnet/tarball/master',
+    #long_description=read('README.md'),
+    license='BSD-3-Clause',
+    packages=find_packages(),
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            '%s = %s.cli.main:main' % (NAME, NAME),
+        ],
+    },
+    )
