f0da0c2f97411256e7aa0d3949234eb458ab44c2
nec4 <nec4@rice.edu>
2019-05-27 18:15:36 -0500

    fixed tests for nnet.py

--- cgnet/tests/test_nnet.py
+++ cgnet/tests/test_nnet.py
@@ -3,29 +3,34 @@ import torch
 import torch.nn as nn
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error as mse
-from network.nnet import *
+from cgnet.network.nnet import Net, LinearLayer, ForceLoss
 
 # Random test data
-x0 = torch.rand((3000, 2), requires_grad=True)
-y0 = torch.rand((3000, 2))
+x0 = torch.rand((3000, 1), requires_grad=True)
+slope = np.random.randn()
+noise = 0.1*torch.rand((3000, 1))
+y0    = torch.ones((3000,1))*(slope + noise)
 batch = {'traj': x0, 'force': y0}
-
 
 def test_linear_layer():
     """Tests LinearLayer function"""
 
     rand = np.random.randint(1, 11)
-    layers = LinearLayer(2, rand, activation=None, bias=True)
+    layers = LinearLayer(1, rand, activation=None, bias=True)
     rands = 3 * [rand]
-    biases = np.random.randint(2, size=3)
-    for r, bias in zip(rands, baises):
+    biases = list(np.random.randint(2, size=3))
+    for r, bias in zip(rands, biases):
         layers += LinearLayer(r, r, activation=nn.ReLU(),
-                              bias=bias, dropout=0.3)
-    layers += LinearLayer(rand, 2, activation=None)
-    for layer in layers:
-        if isinstance(layer, type(nn.Linear())):
-            np.testing.assert_equal(bool(layer.bias), bool(bias))
-
+                              bias=bool(bias), dropout=0.3)
+    layers += LinearLayer(rand, 1, activation=None,bias=False)
+    biases = [1] + biases + [0]
+    linears = [l for l in layers if isinstance(l,nn.Linear)]
+    for layer, bias in zip(linears,biases):
+        if isinstance(layer, nn.Linear):
+            if layer.bias is not None:
+                np.testing.assert_equal(bool(layer.bias.data[0]), bool(bias))
+            else:
+                np.testing.assert_equal(bool(layer.bias), bool(bias))
     seq = nn.Sequential(*layers)
     y = seq(x0)
 
@@ -34,16 +39,15 @@ def test_linear_layer():
 
 def test_net():
     """Tests Net class"""
-    rand = np.random.randint(10)
-    arch = LinearLayer(2, rand, bias=True, activation=nn.Tanh())\
+    rand = np.random.randint(1,10)
+    arch = LinearLayer(1, rand, bias=True, activation=nn.Tanh())\
         + LinearLayer(rand, rand, bias=True, activation=nn.Tanh())\
         + LinearLayer(rand, rand, bias=True, activation=nn.Tanh())\
         + LinearLayer(rand, 1, bias=True, activation=nn.Tanh())\
 
     model = Net(arch, ForceLoss())
     np.testing.assert_equal(len(arch), model.arch.__len__())
-    np.testing.assert_equal(isinstance(ForceLoss(), type(nn.Module())),
-                            isinstance(model.criterion, type(nn.Module())))
+    np.testing.assert_equal(True, isinstance(model.criterion, ForceLoss))
 
     energy, force = model.forward(x0)
     np.testing.assert_equal(energy.size(), (x0.size()[0], 1))
@@ -53,10 +57,11 @@ def test_net():
 def test_linear_regression():
     """Comparison of single layer network for linear regression with sklearn"""
 
-    layers = LinearLayer(2, 1, activation=None, bias=True)
+    layers = LinearLayer(1, 1, activation=None, bias=True)
     model = Net(layers, ForceLoss())
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-    epochs = 500
+    print(model)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    epochs = 1000
     for i in range(epochs):
         optimizer.zero_grad()
         U, F = model.forward(x0)
@@ -66,11 +71,11 @@ def test_linear_regression():
     loss = loss.data.numpy()
 
     # sklearn model for comparison
-
-    lrg = LinearRegression()
     x = x0.detach().numpy()
     y = y0.numpy()
+
+    lrg = LinearRegression()
     reg = lrg.fit(x, y)
     y_pred = reg.predict(x)
 
-    np.testing.assert_almost_equal(mse(y, y_pred), loss, decimal=3)
+    np.testing.assert_almost_equal(mse(y,y_pred), loss, decimal=3)
