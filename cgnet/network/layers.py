b41e8d62ca8bb6d7b5b8ec0ddb6a1aed9d301436
nec4 <nec4@rice.edu>
2019-06-07 19:39:51 -0500

    Renamed priors.py to layers.py. Moved LinearLayer to layers.py

--- cgnet/network/layers.py
+++ cgnet/network/layers.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 class PriorLayer(nn.Module):
     """Layer for adding prior energy computations external to CGnet hidden
     output
@@ -42,10 +43,10 @@ class PriorLayer(nn.Module):
                 else:
                     self.start_idx += num
             for key, par in feat_data.items():
-                 self.features.append(key)
-                 self.feat_idx.append(self.start_idx +
-                                      descriptions[self.feature_type].index(key))
-                 self.params.append(par)
+                self.features.append(key)
+                self.feat_idx.append(self.start_idx +
+                                     descriptions[self.feature_type].index(key))
+                self.params.append(par)
 
     def forward(self, in_feat):
         raise NotImplementedError
@@ -70,8 +71,8 @@ class RepulsionLayer(PriorLayer):
         self.repulsion_parameters = torch.tensor([])
         for param_dict in self.params:
             self.repulsion_parameters = torch.cat((self.repulsion_parameters,
-                       torch.tensor([[param_dict['ex_vol']],
-                       [param_dict['exp']]])),dim=1)
+                                        torch.tensor([[param_dict['ex_vol']],
+                                        [param_dict['exp']]])), dim=1)
 
     def forward(self, in_feat):
         """Calculates repulsion interaction contributions to energy
@@ -87,9 +88,9 @@ class RepulsionLayer(PriorLayer):
         """
 
         n = len(in_feat)
-        energy = torch.sum((self.repulsion_parameters[0,:]/in_feat)
-                            ** self.repulsion_parameters[1,:],
-                            1).reshape(n, 1) / 2
+        energy = torch.sum((self.repulsion_parameters[0, :]/in_feat)
+                           ** self.repulsion_parameters[1, :],
+                           1).reshape(n, 1) / 2
         return energy
 
 
@@ -120,8 +121,8 @@ class HarmonicLayer(PriorLayer):
         self.harmonic_parameters = torch.tensor([])
         for param_dict in self.params:
             self.harmonic_parameters = torch.cat((self.harmonic_parameters,
-                                           torch.tensor([[param_dict['k']],
-                                           [param_dict['mean']]])), dim=1)
+                                       torch.tensor([[param_dict['k']],
+                                       [param_dict['mean']]])), dim=1)
 
     def forward(self, in_feat):
         """Calculates harmonic contribution of bond/angle interactions to energy
@@ -140,7 +141,82 @@ class HarmonicLayer(PriorLayer):
         """
 
         n = len(in_feat)
-        energy = torch.sum(self.harmonic_parameters[0, :] * (in_feat -
-                           self.harmonic_parameters[1, :]) ** 2,
+        energy = torch.sum(self.harmonic_parameters[0, :] *
+                           (in_feat - self.harmonic_parameters[1, :]) ** 2,
                            1).reshape(n, 1) / 2
         return energy
+
+
+def LinearLayer(
+        d_in,
+        d_out,
+        bias=True,
+        activation=None,
+        dropout=0,
+        weight_init='xavier',
+        weight_init_args=None,
+        weight_init_kwargs=None):
+    """Linear layer function
+
+    Parameters
+    ----------
+    d_in : int
+        input dimension
+    d_out : int
+        output dimension
+    bias : bool (default=True)
+        specifies whether or not to add a bias node
+    activation : torch.nn.Module() (default=None)
+        activation function for the layer
+    dropout : float (default=0)
+        if > 0, a dropout layer with the specified dropout frequency is
+        added after the activation.
+    weight_init : str, float, or nn.init function (default=\'xavier\')
+        specifies the initialization of the layer weights. For non-option
+        initializations (eg, xavier initialization), a string may be used
+        for simplicity. If a float or int is passed, a constant initialization
+        is used. For more complicated initializations, a torch.nn.init function
+        object can be passed in.
+    weight_init_args : list or tuple (default=None)
+        arguments (excluding the layer.weight argument) for a torch.nn.init
+        function.
+    weight_init_kwargs : dict (default=None)
+        keyword arguements for a torch.nn.init function
+
+    Returns
+    -------
+    seq : list of torch.nn.Module() instances
+        the full linear layer, including activation and optional dropout.
+
+    Example
+    -------
+    MyLayer = LinearLayer(5,10,bias=True,activation=nn.Softplus(beta=2),
+                               weight_init=nn.init.kaiming_uniform_,
+                               weight_init_kwargs={"a":0,"mode":"fan_out",
+                               "nonlinearity":"leaky_relu"})
+
+    Produces a linear layer with input dimension 5, output dimension 10, bias
+    inclusive, followed by a beta=2 softplus activation, with the layer weights
+    intialized according to kaiming uniform procedure with preservation of weight
+    variance magnitudes during backpropagation.
+
+    """
+
+    seq = [nn.Linear(d_in, d_out, bias=bias)]
+    if activation:
+        seq += [activation]
+    if dropout:
+        seq += [nn.Dropout(dropout)]
+    if weight_init == 'xavier':
+        torch.nn.init.xavier_uniform_(seq[0].weight)
+    if weight_init == 'identity':
+        torch.nn.init.eye_(seq[0].weight)
+    if isinstance(weight_init, int) or isinstance(weight_init, float):
+        torch.nn.init.constant_(seq[0].weight, weight_init)
+    if callable(weight_init):
+        if weight_init_args is None:
+            weight_init_args = []
+        if weight_init_kwargs is None:
+            weight_inti_kwargs = []
+        weight_init(seq[0].weight, *weight_init_args, **weight_init_kwargs)
+    return seq
