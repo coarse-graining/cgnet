dd8899a6f6b414e503ca4e94545b87b2b192d0b1
Brooke Husic <bhusic@stanford.edu>
2019-07-12 10:13:49 +0200

    moved divergence tests to new file and changed names

--- cgnet/tests/test_protein_statistics.py
+++ cgnet/tests/test_protein_statistics.py
@@ -6,9 +6,6 @@ import torch
 
 from cgnet.feature import ProteinBackboneFeature
 from cgnet.feature import ProteinBackboneStatistics
-from cgnet.feature import compute_KLdivergence
-from cgnet.feature import compute_JSdivergence
-from cgnet.feature import compute_intersection
 
 frames = np.random.randint(1, 10)
 beads = np.random.randint(4, 10)
@@ -114,68 +111,3 @@ def test_bondconst_dict_2():
             assert len(bondconst_dict[k]) == n_keys_bondconst
         else:
             assert len(bondconst_dict[k]) == n_keys
-
-
-def test_compute_KLdivergence():
-    # Tests the calculation of KL divergence for histograms drawn from
-    # uniform distributions
-    nbins = np.random.randint(0, high=50)
-    bins = np.linspace(0, 1, nbins)
-    hist1, bins = np.histogram(np.random.uniform(size=nbins), bins=bins,
-                               density=True)
-    hist2, bins = np.histogram(np.random.uniform(size=nbins), bins=bins,
-                               density=True)
-
-    div = compute_KLdivergence(hist1, hist1)
-    np.testing.assert_allclose(0, div)
-
-    hist1 = np.ma.masked_where(hist1 == 0, hist1)
-    hist2 = np.ma.masked_where(hist2 == 0, hist2)
-    summand = hist1 * np.ma.log(hist1/hist2)
-    div_0 = np.ma.sum(summand)
-    div = compute_KLdivergence(hist1, hist2)
-    assert div_0 == div
-
-def test_compute_JSdivergence():
-    # Tests the calculation of SL divergence for histograms drawn from
-    # uniform distributions
-    nbins = np.random.randint(0, high=50)
-    bins = np.linspace(0, 1, nbins)
-    hist1, bins = np.histogram(np.random.uniform(size=nbins), bins=bins,
-                               density=True)
-    hist2, bins = np.histogram(np.random.uniform(size=nbins), bins=bins,
-                               density=True)
-
-    div = compute_JSdivergence(hist1, hist1)
-    np.testing.assert_allclose(0, div)
-
-    hist1 = np.ma.masked_where(hist1 == 0, hist1)
-    hist2 = np.ma.masked_where(hist2 == 0, hist2)
-    mix = 0.5 * (hist1 + hist2)
-    summand = 0.5 * (hist1 * np.ma.log(hist1/mix))
-    summand += 0.5 * (hist2 * np.ma.log(hist2/mix))
-    div_0 = np.ma.sum(summand)
-    div = compute_JSdivergence(hist1, hist2)
-    assert div_0 == div
-
-def test_compute_intersection():
-    # Tests the calculation of intersection for histograms drawn from
-    # uniform distributions
-    nbins = np.random.randint(0, high=50)
-    bins = np.linspace(0, 1, nbins)
-    hist1, bins = np.histogram(np.random.uniform(size=nbins), bins=bins,
-                               density=True)
-    hist2, bins = np.histogram(np.random.uniform(size=nbins), bins=bins,
-                               density=True)
-
-    inter = compute_intersection(hist1, hist1, bins)
-    np.testing.assert_allclose(1, inter)
-
-    inter_0 = 0.00
-    intervals = np.diff(bins)
-    for i in range(len(intervals)):
-        inter_0 += min(intervals[i] * hist1[i], intervals[i] * hist2[i])
-    inter = compute_intersection(hist1, hist2, bins)
-    assert inter_0 == inter
-
-
