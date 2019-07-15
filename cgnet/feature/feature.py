555eb366514749a2e6b32dc8a0ad4a2048d8102a
Dominik Lemm <lemm.d@web.de>
2019-07-15 15:41:03 +0200

    implementation of continuous convolution and interaction block

--- cgnet/feature/feature.py
+++ cgnet/feature/feature.py
@@ -1,10 +1,12 @@
-# Author: B Husic
+# Author: Brooke Husic, Dominik Lemm
 # Contributors: Jiang Wang
 
 
 import torch
 import torch.nn as nn
-import numpy as np
+
+from cgnet.feature.utils import ShiftedSoftplus
+from cgnet.network.layers import LinearLayer
 
 
 class ProteinBackboneFeature(nn.Module):
@@ -124,3 +126,186 @@ class ProteinBackboneFeature(nn.Module):
             self.description_order.append('Dihedral_sines')
 
         return out
+
+
+class ContinuousFilterConvolution(nn.Module):
+    """
+    Continuous-filter convolution block as described by Schütt et al. (2018).
+
+    Unlike convential convolutional layers that utilize discrete filter tensors,
+    a continuous-filter convolutional layer evaluates the convolution at discrete
+    locations in space using continuous radial filters (Schütt et al. 2018).
+
+    A continuous-filter convolution block consists of a filter generating network
+    as follows:
+
+    Filter Generator:
+        1. Featurization of cartesian positions into distances
+           (which are roto-translationally invariant)
+           (already precomputed so will be parsed as arguments)
+        2. Atom-wise/Linear layer with shifted-softplus activation function
+        3. Atom-wise/Linear layer with shifted-softplus activation function
+
+    The filter is then multiplied element-wise with the feature input to form
+    a residual connection.
+
+    Parameters
+    ----------
+    num_gaussians: int
+        Number of Gaussians that has been used in the radial basis function.
+        Needed in to determine the input feature size of the first dense layer.
+    num_filters: int
+        Number of filters that will be created. Also determines the output size.
+        Needs to be the same size as the features of the residual connection.
+
+    References
+    ----------
+    K.T. Schütt. P.-J. Kindermans, H. E. Sauceda, S. Chmiela,
+        A. Tkatchenko, K.-R. Müller. (2018)
+        SchNet - a deep learning architecture for molecules and materials.
+        The Journal of Chemical Physics.
+        https://doi.org/10.1063/1.5019779
+    """
+
+    def __init__(self, num_gaussians, num_filters):
+        super(ContinuousFilterConvolution, self).__init__()
+        filter_layers = LinearLayer(num_gaussians, num_filters, bias=True,
+                                    activation=ShiftedSoftplus())
+        # In SchNetPack they don't use any activation here, but in the
+        # publication figures there is.
+        filter_layers += LinearLayer(num_filters, num_filters, bias=True,
+                                     activation=ShiftedSoftplus())
+        self.filter_generator = nn.Sequential(*filter_layers)
+
+    def forward(self, features, rbf_expansion, neighbor_list):
+        """ Compute convolutional block
+
+        Parameters
+        ----------
+        features: torch.Tensor
+            Feature vector of size [n_examples, n_beads, n_features].
+        rbf_expansion: torch.Tensor
+            Gaussian expansion of atomic distances of size
+            [n_examples, n_beads, n_neighbors, n_gaussians].
+        neighbor_list: torch.Tensor
+            Indices of all neighbors of each bead.
+            Size [n_examples, n_beads, n_neighbors]
+
+        Returns
+        -------
+        conv_features: torch.Tensor
+            Residual features of size [n_examples, n_beads, n_features]
+
+        """
+
+        # Generate the convolutional filter
+        # Shape (n_batch, n_beads, n_neighbors, n_features)
+        conv_filter = self.filter_generator(rbf_expansion)
+
+        # Feature tensor needs to be transformed from
+        # (n_batch, n_beads, n_features)
+        # to
+        # (n_batch, n_beads, n_neighbors, n_features)
+        # This can be done by feeding the features of a respective bead into
+        # its position in the neighbor_list.
+
+        num_batch, num_beads, num_neighbors = neighbor_list.size()
+        # Shape (n_batch, n_beads * n_neighbors, 1)
+        neighbor_list = neighbor_list.reshape(-1, num_beads * num_neighbors, 1)
+        # Shape (n_batch, n_beads * n_neighbors, n_features)
+        neighbor_list = neighbor_list.expand(-1, -1, features.size(2))
+
+        # Gather the features into the respective places in the neighbor list
+        neighbor_features = torch.gather(features, 1, neighbor_list)
+        # Reshape back to (n_batch, n_beads, n_neighbors, n_features) for
+        # element-wise multiplication with the filter
+        neighbor_features = neighbor_features.reshape(num_batch, num_beads,
+                                                      num_neighbors, -1)
+
+        # Element-wise multiplication of the features with
+        # the convolutional filter
+        conv_features = neighbor_features * conv_filter
+
+        # Aggregate/pool the features from (n_batch, n_beads, n_neighs, n_feats)
+        # to (n_batch, n_beads, n_features)
+        agg_features = torch.sum(conv_features, dim=2)
+        return agg_features
+
+
+class InteractionBlock(nn.Module):
+    """
+    SchNet interaction block as described by Schütt et al. (2018).
+
+    A interaction block consists of:
+        1. Atom-wise/Linear layer without activation function
+        2. Continuous filter convolution, which is a filter-generator multiplied
+           element-wise with the output of the previous layer
+        3. Atom-wise/Linear layer with activation
+        4. Atom-wise/Linear layer without activation
+
+    Parameters
+    ----------
+    num_inputs: int
+        Number of input features. Determines input size for the initial linear
+        layer.
+    num_gaussians: int
+        Number of Gaussians that has been used in the radial basis function.
+        Needed in to determine the input size of the continuous filter
+        convolution.
+    num_filters: int
+        Number of filters that will be created in the continuous filter convolution.
+        The same feature size will be used for the output linear layers of the
+        interaction block.
+
+    References
+    ----------
+    K.T. Schütt. P.-J. Kindermans, H. E. Sauceda, S. Chmiela,
+        A. Tkatchenko, K.-R. Müller. (2018)
+        SchNet - a deep learning architecture for molecules and materials.
+        The Journal of Chemical Physics.
+        https://doi.org/10.1063/1.5019779
+    """
+
+    def __init__(self, num_inputs, num_gaussians, num_filters):
+        super(InteractionBlock, self).__init__()
+
+        self.inital_dense = nn.Sequential(
+            *LinearLayer(num_inputs, num_filters, bias=False,
+                         activation=None))
+        self.cfconv = ContinuousFilterConvolution(num_gaussians=num_gaussians,
+                                                  num_filters=num_filters)
+        output_layers = LinearLayer(num_filters, num_filters, bias=True,
+                                    activation=ShiftedSoftplus())
+        output_layers += LinearLayer(num_filters, num_filters, bias=True,
+                                     activation=None)
+        self.output_dense = nn.Sequential(*output_layers)
+
+    def forward(self, features, rbf_expansion, neighbor_list):
+        """ Compute interaction block
+
+        Parameters
+        ----------
+        features: torch.Tensor
+            Input features from an embedding or interaction layer.
+            Shape [n_batch, n_beads, n_features]
+        rbf_expansion: torch.Tensor
+            Radial basis function expansion of interatomic distances.
+            Shape [n_batch, n_beads, n_neighbors, n_gaussians]
+        neighbor_list: torch.Tensor
+            Indices of all neighbors of each bead.
+            Size [n_examples, n_beads, n_neighbors]
+
+        Returns
+        -------
+        output_features: torch.Tensor
+            Output of an interaction block. This output can be used to form
+            a residual connection with the output of a prior embedding/interaction
+            layer.
+            Shape [n_batch, n_beads, n_filters]
+
+        """
+        init_feature_output = self.inital_dense(features)
+        conv_output = self.cfconv(init_feature_output, rbf_expansion,
+                                  neighbor_list)
+        output_features = self.output_dense(conv_output)
+        return output_features
