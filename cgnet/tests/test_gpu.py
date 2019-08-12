1ea72d19e52f6de4a79492d82c6dbc5a61f8d41e
nec4 <nec4@rice.edu>
2019-08-12 18:12:36 -0500

    Fixed test_gpu.py

--- cgnet/tests/test_gpu.py
+++ cgnet/tests/test_gpu.py
@@ -6,10 +6,10 @@ import torch
 import torch.nn as nn
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error as mse
-from cgnet.network import (CGnet, LinearLayer, ForceLoss,
+from cgnet.network import (CGnet, ForceLoss,
                            RepulsionLayer, HarmonicLayer, ZscoreLayer)
-from cgnet.feature import (ProteinBackboneStatistics, ProteinBackboneFeature,
-                           MoleculeDataset)
+from cgnet.feature import (GeometryStatistics, GeometryFeature,
+                           MoleculeDataset, LinearLayer)
 from torch.utils.data import DataLoader
 from nose.exc import SkipTest
 
@@ -20,30 +20,26 @@ width = np.random.randint(2, high=10)
 
 
 def test_model_gpu_mount():
+    # Tests to see if forwarding through CGnet mounted on GPU runs properly
     if not torch.cuda.is_available():
         raise SkipTest("GPU not available for testing.")
     coords = np.random.randn(num_examples, num_beads, 3).astype('float32')
     forces = np.random.randn(num_examples, num_beads, 3).astype('float32')
-    stats = ProteinBackboneStatistics(coords)
+    stats = GeometryStatistics(coords)
+    feat_layer = GeometryFeature(feature_tuples=stats.feature_tuples)
 
-    bondsdict = stats.get_bond_constants(flip_dict=True, zscores=True)
-    bonds = dict((k, bondsdict[k])
-                 for k in [(i, i+1) for i in range(num_beads-1)])
+    bonds_idx = stats.return_indices('Bonds')
+    bonds_list = stats.get_prior_statistics(features='Bonds', as_list=True)
 
-    repul_distances = [i for i in stats.descriptions['Distances']
+    repul_tuples = [i for i in stats.descriptions['Distances']
                        if abs(i[0]-i[1]) > 2]
-    ex_vols = np.random.uniform(2, 8, len(repul_distances))
-    exps = np.random.randint(1, 6, len(repul_distances))
-    repul_dict = dict((index, {'ex_vol': ex_vol, 'exp': exp})
-                      for index, ex_vol, exp
-                      in zip(repul_distances, ex_vols, exps))
+    repul_idx = stats.return_indices(features=repul_tuples)
+    ex_vols = np.random.uniform(2, 8, len(repul_idx))
+    exps = np.random.randint(1, 6, len(repul_idx))
+    repul_list = [{'ex_vol' : ex_vol, 'exp' : exp}
+                  for ex_vol, exp in zip(ex_vols, exps)]
 
-    descriptions = stats.descriptions
-    nums = [len(descriptions['Distances']), len(descriptions['Angles']),
-            len(descriptions['Dihedral_cosines']),
-            len(descriptions['Dihedral_sines'])]
-    descs = [key for key in descriptions.keys()]
-    zscores = stats.get_zscores(tensor=True, as_dict=False).float()
+    zscores = stats.get_zscore_array()
 
     device = torch.device('cuda')
     dataset = MoleculeDataset(coords, forces, device=device)
@@ -52,11 +48,10 @@ def test_model_gpu_mount():
     arch += LinearLayer(sum(nums), width)
     arch += LinearLayer(width, 1)
 
-    priors = [HarmonicLayer(bonds, descriptions, "Distances")]
-    priors += [RepulsionLayer(repul_dict, descriptions,
-                              "Distances")]
+    priors = [HarmonicLayer(bonds_list, bonds_idx)]
+    priors += [RepulsionLayer(repul_list, repul_idx)]
 
-    model = CGnet(arch, ForceLoss(), feature=ProteinBackboneFeature(),
+    model = CGnet(arch, ForceLoss(), feature=feat_layer,
                   priors=priors).float().to(device)
     for _, batch in enumerate(loader):
         coords, forces = batch
