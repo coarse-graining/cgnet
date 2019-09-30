2ca55c3784488d02d2a645039b9a3249699a8079
Brooke Husic <bhusic@stanford.edu>
2019-09-30 19:59:07 +0200

    dataset accommodates embeddings; NOT tested or incorporated into any schnet stuff

--- cgnet/feature/dataset.py
+++ cgnet/feature/dataset.py
@@ -18,8 +18,10 @@ class MoleculeDataset(Dataset):
         Coordinate data of dimension [n_frames, n_beads, n_dimensions]
     forces : np.array
         Coordinate data of dimension [n_frames, n_beads, n_dimensions]
+    embeddings : np.array
+        Embedding data of dimension [n_frames, n_beads, n_embedding_properties]
     selection : np.array (default=None)
-        Array of indices to select from the coordinates and forces.
+        Array of frame indices to select from the coordinates and forces.
         If None, all are used.
     stride : int (default=1)
         Subsample the data by 1 / stride.
@@ -28,40 +30,71 @@ class MoleculeDataset(Dataset):
         Default device is the local CPU.
     """
 
-    def __init__(self, coordinates, forces, selection=None, stride=1,
-                 device=torch.device('cpu')):
+    def __init__(self, coordinates, forces, embeddings=None, selection=None,
+                 stride=1, device=torch.device('cpu')):
         self.stride = stride
-        self.coordinates, self.forces = self._make_arrays(coordinates,
-                                                           forces, selection)
 
+        self.coordinates = self._make_array(coordinates, selection)
+        self.forces = self._make_array(forces, selection)
+        if embeddings is not None:
+            self.embeddings = self._make_array(embeddings, selection)
+        else:
+            self.embeddings = None
+
+        self._check_size_consistency()
         self.len = len(self.coordinates)
         self.device = device
 
     def __getitem__(self, index):
-        return (torch.tensor(self.coordinates[index], requires_grad=True,
-                device=self.device), torch.tensor(self.forces[index],
-                device=self.device))
+        if self.embeddings is None:
+            return (
+                    torch.tensor(self.coordinates[index],
+                        requires_grad=True, device=self.device),
+                    torch.tensor(self.forces[index],
+                        device=self.device)
+                    )
+        else:
+            return (
+                    torch.tensor(self.coordinates[index],
+                        requires_grad=True, device=self.device),
+                    torch.tensor(self.forces[index],
+                        device=self.device),
+                    torch.tensor(self.embeddings[index],
+                        requires_grad=True, device=self.device) # do embeddings require grad?
+                    )
 
     def __len__(self):
         return self.len
 
-    def _make_arrays(self, coordinates, forces, selection=None):
+    def _make_array(self, data, selection=None):
+        """Returns an array that contains a selection of data
+        if specified, at the stride provided.
+        """
         if selection is not None:
-            coordinates = np.array(coordinates[selection][::self.stride])
-            forces = np.array(forces[selection][::self.stride])
+            return np.array(data[selection][::self.stride])
         else:
-            coordinates = np.array(coordinates[::self.stride])
-            forces = np.array(forces[::self.stride])
+            return data[::self.stride]
 
-        if len(coordinates) != len(forces):
-            raise ValueError("Coordinates and forces must have equal lengths")
+    def add_data(self, coordinates, forces, embeddings=None, selection=None):
+        new_coords = self._make_array(coordinates, selection)
+        new_forces = self._make_array(forces, selection)
+        if embeddings is not None:
+            new_embeddings = self._make_array(embeddings, selection)
 
-        return coordinates, forces
-
-    def add_data(self, coordinates, forces, selection=None):
-        new_coords, new_forces = self._make_arrays(coordinates, forces,
-                                                    selection)
         self.coordinates = np.concatenate([self.coordinates, new_coords], axis=0)
         self.forces = np.concatenate([self.forces, new_forces], axis=0)
-
+        if self.embeddings is not None:
+            self.embeddings = np.concatenate([self.embeddings, new_embeddings],
+                                             axis=0)
+        self._check_size_consistency()
         self.len = len(self.coordinates)
+
+    def _check_size_consistency(self):
+        if len(self.coordinates) != len(self.forces):
+            raise ValueError("Coordinates and forces must have equal lengths")
+
+        if self.embeddings is not None:
+            if len(self.coordinates) != len(self.embeddings):
+                raise ValueError(
+                "Coordinates, forces, and embeddings must have equal lengths"
+                                )
