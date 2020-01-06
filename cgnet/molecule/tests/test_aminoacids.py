77c01d040ef460c463c5ab25fd1f7e83763af122
Brooke Husic <bhusic@stanford.edu>
2020-01-06 16:08:52 +0100

    more aminos tests

--- cgnet/molecule/tests/test_aminoacids.py
+++ cgnet/molecule/tests/test_aminoacids.py
@@ -7,6 +7,8 @@ from cgnet.molecule import (CGMolecule, RESIDUE_RADII,
                             calculate_hard_sphere_minima)
 
 def test_angstrom_conversion():
+    # This tests in a somewhat roundabout way whether the angstrom
+    # conversion from the master dictionary is correct 
 
     # Create a CG molecule with each amino acid doubled
     all_residues = list(RESIDUE_RADII.keys())
@@ -23,7 +25,9 @@ def test_angstrom_conversion():
 
     # Calculate the minima with a prefactor of 1.0
     same_res_minima = calculate_hard_sphere_minima(same_res_bonds,
-                                                   mypeptide, prefactor=1.0)
+                                                   mypeptide,
+                                                   units='Angstroms',
+                                                   prefactor=1.0)
 
     # The minima should be the radii in nanometers after a factor of 1/20
     single_nm_radii = [i/20 for i in same_res_minima]
@@ -33,7 +37,43 @@ def test_angstrom_conversion():
 
     np.testing.assert_allclose(values_from_dict, single_nm_radii)
 
-# def test_minima_calculations():
+
+def test_minima_calculation_valuess():
+    # This is a manual test of the minima calculations
+
+    # Shuffle the twenty amino acids to make a peptide from
+    possible_residues = list(RESIDUE_RADII.keys())
+    np.random.shuffle(possible_residues)
+
+    # Make a CA only CGMolecule object with a random number of residues
+    num_residues = np.random.randint(3, 10)
+    names = ['CA'] * num_residues
+    resseq = np.arange(1, num_residues+1)
+    resmap = {i+1 : possible_residues[i] for i in range(num_residues)}
+    mypeptide = CGMolecule(names, resseq, resmap)
+
+    # Enumerate all the bonds
+    bonds = list(itertools.combinations(np.arange(num_residues), 2))
+
+    # Designate a random prefactor
+    prefactor = np.random.uniform(0.5, 1.3)
+
+    # Perform the manual calculation
+    manual_distances = []
+    for bond in bonds:
+        # The +1 is needed because the resmap isn't zero-indexed
+        rad1 = RESIDUE_RADII[resmap[bond[0] + 1]]
+        rad2 = RESIDUE_RADII[resmap[bond[1] + 1]]
+        # The *10 converts to angstroms
+        manual_distances.append(prefactor*rad1*10 + prefactor*rad2*10)
+
+    # Perform the automatic calculation
+    distances = calculate_hard_sphere_minima(bonds, mypeptide,
+                                             prefactor=prefactor)
+
+    # The high tolerance is due to the SFs in the master list
+    np.testing.assert_allclose(manual_distances, distances, rtol=1e-4)
+
 
 def test_CA_vs_CB_minima_correspondence():
     # This tests that CA-CA distances are the same as CB-CB for the same
@@ -64,3 +104,29 @@ def test_CA_vs_CB_minima_correspondence():
 
     # Ensure equality
     np.testing.assert_array_equal(CA_CA_minima, CB_CB_minima)
+
+
+def test_intra_residue_zeros():
+    # This tests that the minimum distance between atoms within the same
+    # residue returns zero
+
+    # Shuffle the twenty amino acids to make a peptide from
+    possible_residues = list(RESIDUE_RADII.keys())
+    np.random.shuffle(possible_residues)
+
+    # Make a CA+CB CGMolecule object with a random number of residues
+    num_residues = np.random.randint(3, 10)
+    names = ['CA', 'CB'] * num_residues
+    resseq = list(np.concatenate([np.repeat(i+1, 2)
+                  for i in range(num_residues)]))
+    resmap = {i+1 : possible_residues[i] for i in range(num_residues)}
+    mypeptide = CGMolecule(names, resseq, resmap)
+
+    # Enumerate the intraresidue CA-CB bonds
+    same_res_bonds = [(i, i+1) for i in range(num_residues - 1) if i % 2 == 0]
+
+    should_be_zeros = calculate_hard_sphere_minima(same_res_bonds, mypeptide)
+
+    # Test that a zero is returned for each residue
+    np.testing.assert_array_equal(should_be_zeros,
+                                  np.zeros(num_residues // 2))
