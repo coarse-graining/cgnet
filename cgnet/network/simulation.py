d049bc5c33302de201f84789c7c994a655743455
Brooke Husic <bhusic@stanford.edu>
2020-05-04 12:05:38 +0200

    small doc changes

--- cgnet/network/simulation.py
+++ cgnet/network/simulation.py
@@ -492,30 +492,30 @@ class Simulation():
     def _save_numpy(self, iter_):
         """Utility to save numpy arrays"""
         key = self._get_numpy_count()
-        self.save_dict[key] = {}
+        self.save_dict[key] = {} # debug
 
         traj_to_export = self.simulated_traj[self._npy_starting_index:iter_]
         traj_to_export = self._swap_and_export(traj_to_export)
-        self.save_dict[key]['traj'] = traj_to_export
-        #np.save("{}_traj_{}.npy".format(self.filename, key), traj_to_export)
+        self.save_dict[key]['traj'] = traj_to_export # debug
+        np.save("{}_traj_{}.npy".format(self.filename, key), traj_to_export)
 
         if self.save_forces:
             forces_to_export = self.simulated_forces[self._npy_starting_index:iter_]
             forces_to_export = self._swap_and_export(forces_to_export)
-            self.save_dict[key]['forces'] = forces_to_export
-            #np.save("{}_forces_{}.npy".format(self.filename, key), forces_to_export)
+            self.save_dict[key]['forces'] = forces_to_export # debug
+            np.save("{}_forces_{}.npy".format(self.filename, key), forces_to_export)
 
         if self.save_potential:
             potentials_to_export = self.simulated_potential[self._npy_starting_index:iter_]
             potentials_to_export = self._swap_and_export(potentials_to_export)
-            self.save_dict[key]['potential'] = potentials_to_export
-            #np.save("{}_potential_{}.npy".format(self.filename, key), potentials_to_export)
+            self.save_dict[key]['potential'] = potentials_to_export # debug
+            np.save("{}_potential_{}.npy".format(self.filename, key), potentials_to_export)
 
         if self.friction is not None:
             kinetic_energies_to_export = self.kinetic_energies[self._npy_starting_index:iter_]
             kinetic_energies_to_export = self._swap_and_export(kinetic_energies_to_export)
-            self.save_dict[key]['kes'] = kinetic_energies_to_export
-            #np.save("{}_ke_{}.npy".format(self.filename, key), kinetic_energies_to_export)
+            self.save_dict[key]['kes'] = kinetic_energies_to_export # debug
+            np.save("{}_ke_{}.npy".format(self.filename, key), kinetic_energies_to_export)
 
         self._npy_starting_index = iter_
         self._npy_file_index += 1
@@ -617,7 +617,9 @@ class Simulation():
             if np.any(np.isnan(x_new.detach().numpy())):
                 if self.save_npys:
                     self._save_numpy(t+1)
-                raise RuntimeError("NaN encountered in simulation; terminating.")
+                raise RuntimeError(
+                    "NaN encountered in simulation; terminating."
+                    )
 
             self._x_new = x_new #debug
             self._v_new = v_new #debug
